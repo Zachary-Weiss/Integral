c35d6cd01377181218c39d513cc6799f4b4a0852
Zachary-Weiss <zweiss1@trinity.edu>
2024-10-18 12:23:07 -0500

    Removed unnecessary code

--- integral.py
+++ integral.py
@@ -3,6 +3,7 @@ import pyautogui
 import time
 import pyperclip
 import random
+import sys
 
 # Define your codes and corresponding texts
 codes = {
@@ -11,41 +12,29 @@ codes = {
 
 # Global variable to hold the current input code
 current_code = ''
-type_speed = 0.01
-stopTyping = False
 
 # Function to handle key presses 
 def on_press(key):
     global current_code 
     try:
         if hasattr(key, 'char'):
-            if key.char == '#' or key.char == '$':  # Trigger with # 
+            if key.char == '#' or key.char == '$':  # Trigger with # or $
                 current_code = ''  # Clear the current code 
             elif key.char is not None:
                 current_code += key.char  # Append character to code 
                 
             if current_code in codes:
-                """
-                if current_code[0] == "#":
-                    # Print and type each character with a random delay
-                    text = codes[current_code]
-                    pyautogui.typewrite((1 + len(current_code)) * "\b")
-                    #pyautogui.typewrite(text)
-                    for char in text:
-                        print(char, end='', flush=True)  # Print character without newline
-                        time.sleep(type_speed)
-                        pyautogui.typewrite(char)  # Type the character"""
-                # Add the text to the clipboard
                 text = codes[current_code]
-                pyautogui.typewrite((1 + len(current_code)) * "\b")
-                pyperclip.copy(text) #
+                pyautogui.typewrite((1 + len(current_code)) * "\b") #delete the code that was just typed, including the special char
+                pyperclip.copy(text) # Add the text to the clipboard
 
-                print()  # Print a newline after the message
+                print()  # Print a newline after the message. This is so you can keep track of the strings you've copied in the terminal
                 current_code = ''  # Reset after typing the text
 
 
         if key == keyboard.Key.esc:  # Exit on ESC key
-            return False  # Stop listener
+            #return False  # Stop listener
+            sys.exit()
 
     except Exception as e:
         print(f"Error: {e}")
